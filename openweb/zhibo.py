bb27777ee27daaf71c5cca3727a91f298ed323cc
august <1@augustor.com>
2021-05-08 21:00:32 +0800

    切换无头模式

--- openweb/zhibo.py
+++ openweb/zhibo.py
@@ -6,9 +6,13 @@ if __name__ == "__main__":
 
     chrome_options = Options()
     chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    
     browser = webdriver.Chrome(
-        'D:/tools/chromedriver/chromedriver.exe')
-    browser.get('https://wx.vzan.com/live/tvchat-328425822?v=1594348316127')
+        '/usr/local/chromedriver', chrome_options=chrome_options)
+
+    browser.get('https://wx.vzan.com/live/tvchat-1286278451?shareuid=300126588#/')
     time.sleep(5)
     
     while 1==1:
