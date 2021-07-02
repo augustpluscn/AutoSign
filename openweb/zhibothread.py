8a49ac4c302e7293edbdfec4441697f192de5b5c
august <1@augustor.com>
2021-07-02 16:36:27 +0800

    1

--- openweb/zhibothread.py
+++ openweb/zhibothread.py
@@ -3,7 +3,7 @@ import time
 import threading
 
 def open():
-    target = "https://wx.vzan.com/live/tvchat-1627924258#/"
+    target = "https://wx.vzan.com/live/tvchat-1096117732#/"
 
     
     while 1==1:
