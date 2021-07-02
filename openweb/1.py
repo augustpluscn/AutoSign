1aaed520c3b7ca19842359d88b52d5d08ad2ce93
august <1@augustor.com>
2021-07-02 21:40:41 +0800

    1

---
+++ openweb/1.py
@@ -0,0 +1,19 @@
+import requests
+import time
+import threading
+
+def open():
+    target = "https://wx.vzan.com/live/tvchat-2096859123#/"
+
+    
+    while 1==1:
+        req = requests.get(url = target)
+        req.encoding = 'utf-8'
+        # print(req.text)
+        time.sleep(2)
+
+
+if __name__ == "__main__":
+    # open()
+    for x in range(10):
+        threading.Thread(target=open).start()
